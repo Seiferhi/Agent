1e1a687cb8e816acc00ac0720c99e17a4ddbcd9c
nour <nourelhoudamejri20@gmail.com>
2019-09-06 22:58:28 +0200

    get request of conseils faild

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import TopBar from "./components/topBar/TopBar";
 import SideBar from "./components/SideBar/SideBar";
 import Annonces from "./components/Annonces/Annonces";
 import { BrowserRouter, Route } from "react-router-dom";
-import AjouterAnnocne from "./components/agent/AjouterAnnonce/AjouterAnnonce";
-import Profil from "./components/agent/profile/Profile";
-import BoiteRec from "./components/agent/boiteDeReception/boiteDeReception";
-import DetaillesMgs from "./components/agent/boiteDeReception/detailleMsg";
-import DemandeVente from "./components/agent/vente/Vente";
-import DemandeLocation from "./components/agent/location/location";
+import AjouterAnnocne from "./components/AjouterAnnonce/AjouterAnnonce";
+import Profil from "./components/profile/Profile";
+import BoiteRec from "./components/boiteDeReception/boiteDeReception";
+import DetaillesMgs from "./components/boiteDeReception/detailleMsg";
+import DemandeVente from "./components/vente/Vente";
+import DemandeLocation from "./components/location/location";
 import Conseils from "./components/Annonces/service/conseils/conseils";
 class App extends Component {
   render() {
--- src/components/Annonces/service/conseils/conseilsTables.js
+++ src/components/Annonces/service/conseils/conseilsTables.js
@@ -1,44 +1,91 @@
-import React, { Component } from "react";
-import MaterialTable from "material-table";
-class PositioningActionsColumn extends React.Component {
-  render() {
-    return (
-      <MaterialTable
-        columns={[
-          { title: "Nom & prénom", field: "nom" },
-          { title: "Adresse Email", field: "email" },
-          { title: "Téléphone ", field: "tel", type: "numeric" },
-          {
-            title: "Message",
-            field: "message",
-            type: "String"
-          }
-        ]}
-        data={[
-          {
-            nom: "Mehmet",
-            email: "Baran",
-            tel: 1987,
-            message:
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et dui vestibulum, bibendum"
-          }
-        ]}
-        actions={[
-          {
-            icon: "save",
-            tooltip: "Save User",
-            onClick: (event, rowData) => alert("You saved " + rowData.name)
-          },
-          {
-            icon: "delete",
-            tooltip: "Delete User",
-            onClick: (event, rowData) => {
-              window.confirm("You want to delete " + rowData.name);
-            }
-          }
-        ]}
-      />
-    );
+import React, { Component } from 'react'
+import { MDBDataTable } from 'mdbreact';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+
+class PositioningActionsColumn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      conseils: ''
+    };
   }
+
+
+
+componentDidMount() {
+axios 
+  .get("localhost:8080/conseils/all")
+  .then(res => {
+    this.setState({ conseils: res.data });
+  })
+  .catch(err => console.log(err));
 }
+
+
+render(){
+  
+  const {conseils} = this.state
+
+  const data = {
+    columns: [
+      {
+        label: 'Nom',
+        field: 'nom',
+        sort: 'asc',
+        width: 150
+      },
+    
+      {
+        label: 'Email',
+        field: 'email',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Telephone',
+        field: 'telephone',
+        sort: 'asc',
+        width: 100
+      },
+      {
+        label: 'Message',
+        field: 'message',
+        sort: 'asc',
+        width: 100
+      }
+    ],
+    rows: [
+    ]
+  };
+  
+  if(conseils)
+  {
+  {conseils.map((conseil,index) =>  
+   conseil.activated ?
+    data.rows.push(
+    {
+     
+      'Nom' :conseil.nom, 
+      'Email ' :conseil.email, 
+      'Telephone' :conseil.tel, 
+      'Message' :conseil.message, 
+     } )  :null
+  
+)}
+  
+  }
+
+  return (
+    <MDBDataTable
+      striped
+      hover
+      data={data}
+    />
+  );
+}
+}
+
 export default PositioningActionsColumn;
